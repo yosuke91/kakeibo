e0108ac4d9da26de7cabe96a887a166c7a61a347
yosuke91 <cyaojie47@gmail.com>
2020-06-10 20:06:27 +0900

    new.view make

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -3,4 +3,18 @@ class IncomesController < ApplicationController
   def index
     @incomes = Income.order(created_at: :asc)
   end
+  
+  def new
+    @income = Income.new()
+  end
+    
+  def show
+  end
+
+  def edit
+  end
+  
+private
+  def set_income
+    @income = Income.find(params[:id])
 end
