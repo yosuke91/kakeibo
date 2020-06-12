0fdcbf90174aac217c0987a8e4f1c12919066a99
yosuke91 <cyaojie47@gmail.com>
2020-06-12 15:15:21 +0900

    commit
    登録、更新、削除作成

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -7,14 +7,41 @@ class IncomesController < ApplicationController
   def new
     @income = Income.new()
   end
-    
+
+  def created
+    @income = Income.new(params[:income])
+    if income.save
+      redirect_to @income, notice: "収入科目を登録しました"
+    else 
+      render "new"
+    end
+  end
+
   def show
   end
 
   def edit
   end
+
+  def update
+    @income.assign_attributes(params[:income])
+    if @income.save
+      redirect_to @income, notice: "収入科目を登録しました"
+    else 
+      render "new"
+    end
+  end
+
+  def destroy
+    @income.destroy
+    redirect_to :incomes, notice: "科目を削除しました"
+  end
+  
+  
   
 private
   def set_income
     @income = Income.find(params[:id])
+  end
+
 end
